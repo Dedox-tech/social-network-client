cdecbae1a83626d35ad473e837d30fc9a64708ce
Diego Murillo <diegomurilloferrer@gmail.com>
2023-05-12 17:35:35 -0500

    Impelement a custom password validator for forms

---
+++ src/app/core/helpers/is-non-alphanumeric.helper.ts
@@ -0,0 +1,14 @@
+// Based in https://stackoverflow.com/questions/4434076/best-way-to-alphanumeric-check-in-javascript
+export function isNonAlphanumeric(character: string): boolean {
+  // UTF-16 representation
+  const codeRepresentation: number = character.charCodeAt(0);
+  if (
+    (codeRepresentation > 47 && codeRepresentation < 58) || // Check for 1-9
+    (codeRepresentation > 64 && codeRepresentation < 91) || // Check for A-Z
+    (codeRepresentation > 96 && codeRepresentation < 123) // Check for a-z
+  ) {
+    return false;
+  }
+
+  return true;
+}
---
+++ src/app/core/validators/password.validator.ts
@@ -0,0 +1,34 @@
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
+import { isNonAlphanumeric } from '../helpers/is-non-alphanumeric.helper';
+
+export function passwordValidator(password: string): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const arrayPassword: string[] = password.split('');
+
+    // Passwords must have at least one uppercase ('A'-'Z').
+    const isUpperCharPresent: boolean = arrayPassword.some(
+      (character) => character === character.toUpperCase()
+    );
+
+    // Passwords must have at least one digit ('0'-'9')
+    const isDigitPresent: boolean = arrayPassword.some((character) =>
+      [0, 1, 2, 3, 4, 5, 6, 7, 8, 9].includes(Number(character))
+    );
+
+    // Passwords must have at least one non alphanumeric character
+    const isAlternativeCharPresent: boolean = arrayPassword.some((character) =>
+      isNonAlphanumeric(character)
+    );
+
+    if (!isUpperCharPresent || !isDigitPresent || !isAlternativeCharPresent) {
+      const validationError: ValidationErrors = {
+        invalidPassword: {
+          value: control.value,
+        },
+      };
+      return validationError;
+    }
+
+    return null;
+  };
+}
